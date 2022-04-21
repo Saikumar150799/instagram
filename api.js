ab79a62367e7748c808d0e4dd1afc5c47945da25
Saikumar150799 <97353614+Saikumar150799@users.noreply.github.com>
2022-04-22 00:39:41 +0530

    Update api.js

--- api.js
+++ api.js
@@ -148,7 +148,7 @@ app.post('/users', (req, res) => {
 // UPDATE USER
 app.put('/users/:user_id', (req, res) => {
     let post = req.body
-    client.query(`UPDATE users SET (avatar,name,active) VALUES('${user.avatar}','${user.name},${user.active} where user_id=${req.params.user_id}'`, (err, result) => {
+    client.query(`UPDATE users SET avatar='${user.avatar}',name='${user.name}',active=${user.active} where user_id=${req.params.user_id}`, (err, result) => {
         if (err) {
             console.log(err)
             res.send(err.message)
